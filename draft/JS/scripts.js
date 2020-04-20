6840c0523061e0f890063f9797d1c53b936efe35
Ashraf Madina <ashraf.madina@asset-map.com>
2020-04-20 17:54:42 -0400

    changes for git host

---
+++ draft/JS/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $(document).ready(function() {      
+       $('#sideNav').sideNav('show');
+       $('#sideNav').sideNav('hide');
+          
+    });
+
+  // login modal toggle 
+        $("#loginBtn").click(function() {
+            $("#loginModal").modal("show");
+
+    });
+
+ });
---
+++ draft/JS/testjs.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  };
