5a4f96983ae30e6732a448289f1c13d47cf9ed46
Ashraf Madina <ashraf.madina@asset-map.com>
2020-05-24 00:33:57 -0400

    scrapped old, re-made into new - first commit

--- JS/testjs.js
+++
@@ -1,8 +0,0 @@
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  };
